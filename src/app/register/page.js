1b79f37dcc8509024c72e8cf0f124484b08402a9
mahatab6 <mahatabcall56@gmail.com>
2025-08-21 22:40:04 +0600

    register page added

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,10 +1,9 @@
 "use client";
-
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const {
     register,
     handleSubmit,
@@ -15,9 +14,20 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className=" p-6 rounded-lg shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Register</h1>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <input
+              type="name"
+              placeholder="name"
+              {...register("name", { required: "Enter your name" })}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
           <div>
             <input
               type="email"
@@ -46,20 +56,20 @@ export default function LoginPage() {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
           >
-            Login
+            Create
           </button>
+          <div className="text-center mt-4 text-gray-700">
+            <p>
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="text-blue-500 font-semibold hover:underline"
+              >
+                Login here
+              </Link>
+            </p>
+          </div>
         </form>
-        <div className="text-center mt-4 text-gray-700">
-          <p>
-            Don't have an account?{" "}
-            <Link
-              href="/register"
-              className="text-blue-500 font-semibold hover:underline"
-            >
-              Create account
-            </Link>
-          </p>
-        </div>
       </div>
     </div>
   );
