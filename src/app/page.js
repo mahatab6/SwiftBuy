1a006ba988b563fc0d43c10aba0696e9d77b5970
mahatab6 <mahatabcall56@gmail.com>
2025-08-21 23:50:11 +0600

    product show on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/layout/Hero";
+import ProductsShow from "./components/layout/ProductsShow";
 
 export default function Home() {
   return (
     
     <div>
       <Hero/>
+      <ProductsShow/>
     </div>
   );
 }
